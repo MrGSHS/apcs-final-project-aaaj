3170556fec330dddd52c7f4a1a940fb333de5fe3
Andrew Luo <aluo8@students.d125.org>
2016-05-12 14:25:32 -0500

    Finished 5 in a Row

--- Board-Games Files/FiveInARowRunner.java
+++ Board-Games Files/FiveInARowRunner.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Write a description of class FiveInARowRunner here.
  * 
@@ -11,6 +11,40 @@ public class FiveInARowRunner
     
     public static void main(String[] args)
     {
+        String str;
+        int row=0, col=0, indexOfComma;
+        System.out.println("Enter the coordinates in the format #,# seperated by a comma with no space");
+        Scanner sc = new Scanner(System.in);
+        FiveInARow hi = new FiveInARow();
         
+        while(hi.gameOver() == -1)
+        {
+            System.out.print("Player " + hi.getPlayer() + ", please make a move: ");
+            str = sc.nextLine();
+            indexOfComma=str.indexOf(",");
+            
+            while(indexOfComma == -1 || Integer.parseInt(str.substring(0,indexOfComma))>18 ||
+            Integer.parseInt(str.substring(0,indexOfComma))<0 ||
+            Integer.parseInt(str.substring(indexOfComma+1))>18 || Integer.parseInt(str.substring(indexOfComma+1))<0)
+            {
+                System.out.print("Invalid move. Player " + hi.getPlayer() + ", please make a legal move: ");
+                str=sc.nextLine();
+                indexOfComma=str.indexOf(",");
+            }
+            
+            row=Integer.parseInt(str.substring(0,indexOfComma));
+            col=Integer.parseInt(str.substring(indexOfComma+1));
+            hi.play(row,col);
+            
+        }
+        
+        if(hi.gameOver()==0)
+        {
+            System.out.println("The game is a tie!");
+        }
+        else
+        {
+            System.out.println("Player " + hi.gameOver() + " wins!");
+        }
     }
 }
