b7e86d955d643a2f07b4fda8e7f832ed4fff16e2
Joshua Yoon <jyoon8@students.d125.org>
2016-05-16 14:37:35 -0500

    a

--- Board-Games Files/UltimateTicTacToe.java
+++ Board-Games Files/UltimateTicTacToe.java
@@ -15,7 +15,21 @@ public class UltimateTicTacToe extends TicTacToeBoard
     public UltimateTicTacToe(){
         ultBoardVals = new TicTacToeBoard();
         ult = new TicTacToeBoard [3][3];
+        
+        for(int row=0; row<ult.length; row++){
+            for(int col=0; col<ult[0].length; col++){
+                ult[row][col] = new TicTacToeBoard();
+            }
+        }
+        
+        availableMoves = new boolean [3][3];
+        
         playNumber = 1;
+    }
+    
+    public int getPlayer()
+    {
+        return (playNumber-1)%2+1;
     }
     
     public int play(int rowUlt, int colUlt, int row, int col){
@@ -30,7 +44,7 @@ public class UltimateTicTacToe extends TicTacToeBoard
                 return -1;
             }
             else{
-                ((ult[rowUlt][colUlt]).getBoard())[row][col] = playNumber;
+                ((ult[rowUlt][colUlt]).getBoard())[row][col] = playNumber%2;
                 playNumber++;
             }
         }
@@ -40,7 +54,7 @@ public class UltimateTicTacToe extends TicTacToeBoard
 
     public void updateBoardVals(){
         for(int row=0; row<ult.length; row++){
-            for(int col=0; col<ult[0].length; row++){
+            for(int col=0; col<ult[0].length; col++){
                 ultBoardVals.play(row, col, ult[row][col].getBoardVal());
             }
         }
@@ -62,60 +76,6 @@ public class UltimateTicTacToe extends TicTacToeBoard
     
     public int gameOver(){
         updateBoardVals();
-        if(ultBoardVals.gameOver()==-1){
-            return -1;
-        }
-        
-        int [][] boardVals = new int [3][3];
-        
-        for(int row=0; row<ult.length; row++){
-            for(int col=0; col<ult[0].length; row++){
-                boardVals[row][col] = ult[row][col].getBoardVal();
-            }
-        }
-        
-        for (int[] row : tictactoe)
-        {
-            if (row[0] == row[1] && row[0] == row[2] && row[0] != 0)
-            {
-                return row[0];
-            }
-            
-        }
-        
-        for(int col = 0; col < tictactoe[0].length; col++)
-        {
-            int num1, num2, num3;
-            num1 = tictactoe[0][col];
-            num2 = tictactoe[1][col];
-            num3 = tictactoe[2][col];
-            if (num1 == num2 && num1 == num3 && num1 != 0)
-            {
-                return num1;
-            }
-        }
-        
-        
-        if(tictactoe[0][0]!=0 && tictactoe[0][0]==tictactoe[1][1] && tictactoe[0][0]==tictactoe[2][2])
-        {
-            return tictactoe[0][0];
-        }
-        if(tictactoe[2][0]!=0 && tictactoe[2][0]==tictactoe[1][1] && tictactoe[2][0]==tictactoe[0][2])
-        {
-            return tictactoe[2][0];
-        }
-        
-        for(int row = 0; row < tictactoe.length; row++)
-        {
-            for (int col = 0; col < tictactoe[0].length; col++)
-            {
-                if (tictactoe [row][col] == 0)
-                {
-                    return -1;
-                }
-            }
-        }
-
-        return 0;
+        return ultBoardVals.gameOver();
     }
 }
