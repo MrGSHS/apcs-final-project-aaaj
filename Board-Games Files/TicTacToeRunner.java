4461782798402df4a8e98f1d7b75edde710382d7
Andrew Luo <aluo8@students.d125.org>
2016-05-16 14:40:22 -0500

    hi

--- Board-Games Files/TicTacToeRunner.java
+++ Board-Games Files/TicTacToeRunner.java
@@ -42,5 +42,7 @@ public class TicTacToeRunner
             System.out.println("Player " + hi.gameOver() + " wins!");
         }
         
+        TicTacToeGUI hello = new TicTacToeGUI();
+        hello.display();
     }
 }
