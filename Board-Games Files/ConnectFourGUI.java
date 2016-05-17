b7bd37ee1ec2fd11fbbe65552002a21a6b5d88f0
thehacker <acao8@students.d125.org>
2016-05-17 14:35:51 -0500

    first commit

--- Board-Games Files/ConnectFourGUI.java
+++ Board-Games Files/ConnectFourGUI.java
@@ -9,10 +9,14 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
-public class ConnectFourGUI
+public class ConnectFourGUI extends MouseAdapter implements ActionListener
 {
     private int width;
     private int height;
+    
+    private JFrame frmConnectfour;
+    private JButton[][] options;
+    private JButton reset;
     
     private boolean over;
     
@@ -32,6 +36,24 @@ public class ConnectFourGUI
     
     public void initialize()
     {
+        //main frame
+        frmConnectfour=new Jframe();
+        frmConnectfour.setTitle("ConnectFour");
+        frmConnectfour.setBounds(100,100,500,550);
+        frmConnectfour.setLayout(new BorderLayout());
+        frmConnectfour.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        //creating button array
+        tiles=newJButton[0][height];
+        for (int i=0; i<tiles[0].length; i++)
+        {
+            tiles[0][i]=new JButton();
+            tiles[0][i].addActionListener(this);
+            tiles[0][i].addMouseListener(this);
+            tiles[0][i].setFocusPainted(false); //removes blue border
+            
+        }
+        
+        frmConnectfour.add(BorderLayout.CENTER);
     }
 }
