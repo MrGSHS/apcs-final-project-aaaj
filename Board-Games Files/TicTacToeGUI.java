8262b1bce78c94c6afc298dabb05af34aa22c9df
Andrew Luo <aluo8@students.d125.org>
2016-05-23 14:18:28 -0500

    hi

--- Board-Games Files/TicTacToeGUI.java
+++ Board-Games Files/TicTacToeGUI.java
@@ -1,51 +1,105 @@
+import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
 /**
- * Write a description of class TicTacToeGUI here.
+ * Write a description of class testApplet here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class TicTacToeGUI extends JPanel
+public class TicTacToeGUI extends Applet implements MouseListener
 {
-    private int WIDTH = 500;
-    private int HEIGHT = 500;
-    private JFrame frame;
-    private JPanel panel;
-    TicTacToeBoard hello;
-    
+    int xpos;
+    int ypos;
+    boolean mouseEntered;
+    boolean[][] squares;
+    boolean rect1Clicked;
+    TicTacToeBoard board = new TicTacToeBoard();
 
-    /**
-     * Constructor for objects of class TicTacToeGUI
-     */
-    public TicTacToeGUI()
+    public void init()
     {
-        
-    }
-    
-    private void initialize()
-    {
-        frame = new JFrame();
-        
-    }
-    
-    
-    
-    public static void main(String[] args)
-    {
-        JPanel panel2 = new JPanel();
-        JButton hi = new JButton("OK");
-        panel2.add(hi);
-        JButton cancelButton = new JButton("Cancel");
-        panel2.add(cancelButton);
-        JFrame frame2 = new JFrame("testing");
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame2.add(panel2);
-        frame2.setSize(300,200);
-        frame2.setVisible(true);
-     
+        addMouseListener(this);
     }
 
-    
+    public void paint(Graphics g)
+    {
+        
+
+        g.setColor(Color.black);
+        g.drawLine(0,200,600,200);
+        g.drawLine(0,400,600,400);
+        g.drawLine(200,0,200,600);
+        g.drawLine(400,0,400,600);
+
+        if(board.gameOver() == -1)
+        {
+            for(int row = 0; row < 3; row ++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (board.getBoard()[row][col] == 1)
+                    {
+                        g.drawLine(25+row*200,25+col*200,175 + row*200, 175 + col*200);
+                        g.drawLine(25+row*200, 175+col*200, 175+ row*200, 25+col*200);
+                    }
+                    else if(board.getBoard()[row][col]==2)
+                    {
+                        g.drawOval(25+row*200,25+col*200,150,150);
+                    }
+
+                }
+
+            }
+        }
+
+        if(board.gameOver() == 0)
+        {
+            g.drawString("It's a tie!",270,290);
+        }
+        if(board.gameOver()==1 || board.gameOver()==2)
+        {
+            g.drawString("Player " + board.gameOver() + " wins!", 265,290);
+        }
+
+    }
+
+    public void mouseClicked (MouseEvent me) 
+    {
+        xpos = me.getX(); 
+        ypos = me.getY();
+
+        int row = xpos/200;
+        int col = ypos/200;
+
+        if(board.availableMoves()[row][col])
+        {
+            board.play(row,col);
+            repaint();
+        }
+        else
+        {
+
+        }
+
+    } 
+
+    public void mouseEntered (MouseEvent me) 
+    {
+        mouseEntered = true; 
+        repaint();
+    } 
+
+    public void mousePressed (MouseEvent me) 
+    {
+    } 
+
+    public void mouseReleased (MouseEvent me)
+    {
+    }  
+
+    public void mouseExited (MouseEvent me) 
+    {
+        mouseEntered = false; 
+        repaint(); 
+    }  
 }
---
+++ Board-Games Files/testApplet.java
@@ -0,0 +1,77 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ * Write a description of class testApplet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class testApplet extends Applet implements MouseListener
+{
+    int xpos;
+    int ypos;
+    boolean mouseEntered;
+    boolean rect1Clicked;
+    TicTacToeBoard board;
+    int rect1xco,rect1yco,rect1width,rect1height;
+    
+    public void init()
+    {
+        rect1xco = 20; 
+        rect1yco = 20; 
+        rect1width = 100; 
+        rect1height = 50;
+        addMouseListener(this);
+    }
+    
+    public void paint(Graphics g)
+    {
+        g.setColor(Color.green);
+        g.fillRect(rect1xco,rect1yco,rect1width,rect1height);
+
+        g.setColor(Color.red);
+         g.drawString("("+xpos+","+ypos+")",xpos,ypos);
+         
+          if (rect1Clicked) g.drawString("You clicked in the Rectangle",20,120); 
+          // else this one 
+          else g.drawString("You clicked outside of the rectangle",20,120);
+
+          if (mouseEntered) g.drawString("Mouse is in the applet area",20,160); 
+          else g.drawString("Mouse is outside the Applet area",20,160); 
+    }
+    
+    
+     public void mouseClicked (MouseEvent me) 
+     {
+          xpos = me.getX(); 
+          ypos = me.getY();
+          
+          if (xpos > rect1xco && xpos < rect1xco+rect1width && ypos >rect1yco &&  
+          ypos < rect1yco+rect1height)  
+          {
+              rect1Clicked = true;
+          }
+         
+           else  
+           rect1Clicked = false; 
+           //show the results of the click 
+           repaint();
+     } 
+     public void mouseEntered (MouseEvent me) 
+     {
+         mouseEntered = true; 
+         repaint();
+     } 
+     public void mousePressed (MouseEvent me) 
+     {
+     } 
+     public void mouseReleased (MouseEvent me)
+     {
+     }  
+     public void mouseExited (MouseEvent me) 
+     {
+          mouseEntered = false; 
+          repaint(); 
+     }  
+}
